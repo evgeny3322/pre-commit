5fd86f8b6f0a8b86dfd3a84851f159aad44f9f64
evgeny3322 <retard_x_98@mail.ru>
2023-01-27 22:25:01 +0300

    test5

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
   extends: [
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
+    "airbnb-typescript",
+    "prettier"
   ],
   root: true,
   env: {
