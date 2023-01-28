411e9853902387ea310e915112b48d1d06a5bf71
evgeny3322 <retard_x_98@mail.ru>
2023-01-28 20:48:38 +0300

    test vfv

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,7 @@ import "src/App.css";
 import RoutesWrapper from "src/routing";
 
 const App = () => {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   return <RoutesWrapper />;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 };
 
 export default App;
