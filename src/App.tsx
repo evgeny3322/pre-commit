dd77c642c9beda5c9af36ebec8ae7e488a30c018
evgeny3322 <retard_x_98@mail.ru>
2023-01-28 20:24:13 +0300

    testt

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,40 @@ import "src/App.css";
 import RoutesWrapper from "src/routing";
 
 const App = () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return <RoutesWrapper />;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
 
 export default App;
